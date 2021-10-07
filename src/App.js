99c7a44efeeaeb3c0301b4a7ea1bb9f5bcec6857
NoBasil12 <83785333+NoBasil12@users.noreply.github.com>
2021-10-07 13:47:08 +0100

    created react weather app
    moved from codesandbox do vs code

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello react</h1>
-        <Weather city="tokyo" />
-      </header>
+      <Weather />
     </div>
   );
 }
