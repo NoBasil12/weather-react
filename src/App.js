c28dded613b3697fe3cec480b063192a57a0f8fd
NoBasil12 <83785333+NoBasil12@users.noreply.github.com>
2021-10-07 13:52:54 +0100

    added author footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ function App() {
   return (
     <div className="App">
       <Weather />
+      <p class="author">
+        <a
+          href="https://github.com/NoBasil12/weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-sourse code{" "}
+        </a>
+        , by Francesca Carai
+      </p>
     </div>
   );
 }
