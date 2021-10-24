84cf2f52b331a16337bf95888380d8c44e792d7e
NoBasil12 <83785333+NoBasil12@users.noreply.github.com>
2021-10-24 23:33:06 +0100

    added forecast

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,43 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("celsius");
-
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-  function fahrenheit() {
-    return (props.celsius * 9) / 5 + 32;
-  }
-  if (unit === "celsius") {
-    return (
-      <div className="WeatherTemperature">
-        <strong>{Math.round(props.celsius)}</strong>
-        <span className="units">
-          <span>°C</span> |{" "}
-          <a href="/" onClick={showFahrenheit}>
-            °F
-          </a>
-        </span>
-      </div>
-    );
-  } else {
-    return (
-      <div className="WeatherTemperature">
-        <strong>{Math.round(fahrenheit())}</strong>
-        <span className="units">
-          <a href="/" onClick={showCelsius}>
-            °C
-          </a>{" "}
-          | <span>°F</span>
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div className="WeatherTemperature">
+      <strong>{Math.round(props.celsius)}</strong>
+      <span className="units">
+        <span>°C</span>
+      </span>
+    </div>
+  );
 }
